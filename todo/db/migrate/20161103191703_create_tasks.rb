97a14463971540b40394fa06748a4bb30722e13c
Kots Olesya <kots.olesya@gmail.com>
2016-11-03 21:20:50 +0200

    rails file

---
+++ todo/db/migrate/20161103191703_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string   :title
+      t.integer  :priority, default: 0
+      t.integer  :status,  default: 0
+      t.timestamps
+    end
+  end
+end
